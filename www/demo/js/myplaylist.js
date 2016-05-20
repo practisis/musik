47dd7f7658ef64fe769fedc31c3fa6034a4c4bb4
fabricio carrion <fabricio@practisis.com>
2016-05-20 13:28:31 -0500

    uno
    uno

---
+++ www/demo/js/myplaylist.js
@@ -0,0 +1,62 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+	{
+        mp3:'mix/Danna Paola - No Es Cierto.mp3',
+        title:'No Es Cierto',
+        artist:'Danna Paola - Noel Shacris',
+        rating:4,
+        buy:'#',
+        price:'',
+        duration:'3:45',
+        cover:'mix/dana.png'
+    },
+	{
+        mp3:'mix/Santiago Cruz - y si te quedas, que.mp3',
+        title:'Y si te quedas que ?',
+        artist:'Santiago Cruz',
+        rating:5,
+        buy:'#',
+        price:'1.99',
+        duration:'3:39',
+        cover:'mix/santiago.png'
+    },
+	{
+        mp3:'mix/Basta con que estes - Riccardo Perotti.mp3',
+        title:'Basta con que estes',
+        artist:'Riccardo Perotti',
+        rating:5,
+        buy:'#',
+        price:'1.99',
+        duration:'4:28',
+        cover:'mix/peroti.png'
+    },
+	{
+        mp3:'mix/Mi Corazon Insiste Jencarlos Canela.mp3',
+        title:'Mi Corazon Insiste',
+        artist:'Jencarlos Canela',
+        rating:5,
+        buy:'#',
+        price:'1.99',
+        duration:'3:58',
+        cover:'mix/jean.png'
+    },
+	{
+        mp3:'mix/Despertare Jorge Moreno.mp3',
+        title:'Despertare',
+        artist:'Jorge Moreno',
+        rating:5,
+        buy:'#',
+        price:'1.99',
+        duration:'3:44',
+        cover:'mix/jor.png'
+    }
+	
+	
+];
